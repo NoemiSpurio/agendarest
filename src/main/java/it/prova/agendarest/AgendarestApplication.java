3241e218ec0bf53c3ae532ae6400cf13ae650edd
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-28 15:03:20 +0100

    implementazione di inserimento di due utenti all'avvio

--- src/main/java/it/prova/agendarest/AgendarestApplication.java
+++ src/main/java/it/prova/agendarest/AgendarestApplication.java
@@ -1,13 +1,55 @@
 package it.prova.agendarest;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.prova.agendarest.model.Ruolo;
+import it.prova.agendarest.model.Utente;
+import it.prova.agendarest.service.ruolo.RuoloService;
+import it.prova.agendarest.service.utente.UtenteService;
+
 @SpringBootApplication
-public class AgendarestApplication {
+public class AgendarestApplication implements CommandLineRunner {
+
+	@Autowired
+	private RuoloService ruoloServiceInstance;
+	@Autowired
+	private UtenteService utenteServiceInstance;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendarestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Administrator", Ruolo.ROLE_ADMIN));
+		}
+
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Classic User", Ruolo.ROLE_CLASSIC_USER) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Classic User", Ruolo.ROLE_CLASSIC_USER));
+		}
+
+		if (utenteServiceInstance.findByUsername("admin") == null) {
+			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
+			admin.setEmail("a.admin@prova.it");
+			admin.getRuoli().add(ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN));
+			utenteServiceInstance.inserisciNuovo(admin);
+			utenteServiceInstance.changeUserAbilitation(admin.getId());
+		}
+
+		if (utenteServiceInstance.findByUsername("user") == null) {
+			Utente classicUser = new Utente("user", "user", "Antonio", "Verdi", new Date());
+			classicUser.setEmail("u.user@prova.it");
+			classicUser.getRuoli()
+					.add(ruoloServiceInstance.cercaPerDescrizioneECodice("Classic User", Ruolo.ROLE_CLASSIC_USER));
+			utenteServiceInstance.inserisciNuovo(classicUser);
+			utenteServiceInstance.changeUserAbilitation(classicUser.getId());
+		}
 	}
 
 }
